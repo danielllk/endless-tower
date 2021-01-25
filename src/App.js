c176d626b3606e0dcdcdd88b098caff52d51ded0
danielllk <danielk2091@gmail.com>
2021-01-25 21:34:22 +0000

    test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import EndGame from "./components/EndGame";
 import PlayerSkills from "./components/PlayerSkills";
 import NextStage from "./components/NextStage";
 
-//test
-
 function App() {
   return (
     <Router>
